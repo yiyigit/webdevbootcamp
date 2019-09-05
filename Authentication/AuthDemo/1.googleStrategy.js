1e325daae77d365c6f4ba7c012ee585eb4063a4a
Yiyi ZHOU <49706913+yiyigit@users.noreply.github.com>
2019-09-05 15:15:12 -0700

    Update 1.googleStrategy.js

--- Authentication/AuthDemo/1.googleStrategy.js
+++ Authentication/AuthDemo/1.googleStrategy.js
@@ -2,14 +2,22 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model')
 
 passport.use(
   new GoogleStrategy({
   //options for the google start-->https://console.developers.google.com/-->create credentials
     clientID:keys.google.clientID
     clientSecret:keys.google.clientSecret
-    callbackURL:'/Authentication/AuthDemo/redirect'
+    callbackURL:'/Authentication/AuthDemo/redirect'    
+  }),(accessToken, refreshToken, profile, done) =>{
+  //passport callback function:to see if profile exists already
+    new User = ({
+      googleId: profile.id
+      username: profile.displayName   
+    }).save().then((newUser)=>{
+      console.log('new user created' + newUser
+   //after saving
+    })
     
-  }),() =>{
-  //passport callback function
   }
