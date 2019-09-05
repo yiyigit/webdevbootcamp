52dce7685cbc228370e3e5c4af8ee080eb3ed3c3
Yiyi ZHOU <49706913+yiyigit@users.noreply.github.com>
2019-09-05 15:15:24 -0700

    Update 1.key.js

--- Authentication/AuthDemo/1.key.js
+++ Authentication/AuthDemo/1.key.js
@@ -5,3 +5,4 @@ module.exports = {
     clientID:"61658923U84975904703-cpi5rg03u48j4nrjji8ehuia.apps.googleusercontent.com"
     clientSecret:1fADJFIOAjfeh39r"
   }};
+mongodb:{dbURL:'mongodb://username:password@ds128eu39.mlab.com:3484/yiyi-oauth-test}
