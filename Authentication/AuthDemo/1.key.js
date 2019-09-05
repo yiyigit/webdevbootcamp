b14271237661fcd252e0f642a042aa03529f8067
Yiyi ZHOU <49706913+yiyigit@users.noreply.github.com>
2019-09-05 10:20:34 -0700

    Create 1.key.js

---
+++ Authentication/AuthDemo/1.key.js
@@ -0,0 +1,7 @@
+//add this file to .gitignore
+
+module.exports = {
+  google:{
+    clientID:"61658923U84975904703-cpi5rg03u48j4nrjji8ehuia.apps.googleusercontent.com"
+    clientSecret:1fADJFIOAjfeh39r"
+  }};
