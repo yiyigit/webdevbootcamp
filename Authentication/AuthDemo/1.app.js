a78b8517654f746323f10134a3bb0baa392d4b75
Yiyi ZHOU <49706913+yiyigit@users.noreply.github.com>
2019-09-05 10:36:25 -0700

    Create 1.app.js

---
+++ Authentication/AuthDemo/1.app.js
@@ -0,0 +1,24 @@
+const app = require('express')
+const passport = require("passport")
+const passportSetup = require('./googleStrategy')
+
+//auth login page
+app.get('/login',(req, res) =>{
+  res.render('login')
+});
+
+//auth logout
+app.get('/logout', (req, res) =>{
+  res.send('logging out')
+});
+
+//auth with google
+app.get('/google', passport.authenticate("google",{
+  scope:['profile','location']
+})
+);
+
+//callback route for google to redirect to
+
+
+module.exports = app
