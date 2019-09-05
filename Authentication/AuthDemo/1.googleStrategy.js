a3af25bff9f77e57f593fb57527fa0b68b0c102f
Yiyi ZHOU <49706913+yiyigit@users.noreply.github.com>
2019-09-05 10:20:47 -0700

    Update and rename googleStrategy.js to 1.googleStrategy.js

--- Authentication/AuthDemo/1.googleStrategy.js
+++ Authentication/AuthDemo/1.googleStrategy.js
@@ -1,12 +1,13 @@
 //npm install two libraries
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
 
 passport.use(
   new GoogleStrategy({
   //options for the google start-->https://console.developers.google.com/-->create credentials
-    clientID:
-    clientSecret:
+    clientID:keys.google.clientID
+    clientSecret:keys.google.clientSecret
     
   }),() =>{
   //passport callback function
