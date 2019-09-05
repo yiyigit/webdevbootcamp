0d560c13c7aff69189df3cbab14b4def4872718d
Yiyi ZHOU <49706913+yiyigit@users.noreply.github.com>
2019-09-05 10:36:32 -0700

    Update 1.googleStrategy.js

--- Authentication/AuthDemo/1.googleStrategy.js
+++ Authentication/AuthDemo/1.googleStrategy.js
@@ -8,6 +8,7 @@ passport.use(
   //options for the google start-->https://console.developers.google.com/-->create credentials
     clientID:keys.google.clientID
     clientSecret:keys.google.clientSecret
+    callbackURL:'/Authentication/AuthDemo/redirect'
     
   }),() =>{
   //passport callback function
